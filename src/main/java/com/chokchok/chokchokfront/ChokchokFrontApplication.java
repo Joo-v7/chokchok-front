27ebbed39ff4c9b0c0dc055228c9e6fd8db8832c
joo <helloworld.v7@gmail.com>
2025-04-18 19:29:53 +0900

    프로젝트 생성

---
+++ src/main/java/com/chokchok/chokchokfront/ChokchokFrontApplication.java
@@ -0,0 +1,13 @@
+package com.chokchok.chokchokfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChokchokFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChokchokFrontApplication.class, args);
+    }
+
+}
